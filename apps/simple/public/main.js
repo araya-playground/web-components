e9160df3740856bab90160d57d726ba0a134a9ac
araya <araya@araya.dev>
2024-01-22 20:05:13 +0900

    Impl. custom-elements with ShadowDOM, TemplateElement

--- apps/simple/public/main.js
+++ apps/simple/public/main.js
@@ -1,1 +1,12 @@
-console.log('hello');
+class MyParagraph extends HTMLElement {
+	constructor() {
+		super();
+		const template = document.getElementById('my-paragraph');
+		const templateContent = template.content;
+
+		const shadowRoot = this.attachShadow({mode: 'open'})
+		shadowRoot.appendChild(templateContent.cloneNode(true));
+	}
+}
+
+customElements.define('my-paragraph', MyParagraph)
