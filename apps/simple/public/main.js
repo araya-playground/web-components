a9874eca62b4d314621d1c3d3fd69993c081af3a
araya <araya@araya.dev>
2024-01-23 23:03:31 +0900

    Set button and use <slot>

--- apps/simple/public/main.js
+++ apps/simple/public/main.js
@@ -1,12 +1,30 @@
+// Implements CustomElement behavior
 class MyParagraph extends HTMLElement {
-	constructor() {
-		super();
-		const template = document.getElementById('my-paragraph');
-		const templateContent = template.content;
+  clickCounter = 0;
+  constructor() {
+    super();
+    // get <template> content
+    const template = document.getElementById("my-paragraph");
+    const templateContent = template.content;
 
-		const shadowRoot = this.attachShadow({mode: 'open'})
-		shadowRoot.appendChild(templateContent.cloneNode(true));
-	}
+    // Define DOM operations for cloned template content
+    const clonedNode = templateContent.cloneNode(true);
+    const containerEl = clonedNode.getElementById("container");
+    const btnEl = clonedNode.getElementById("switch-color-btn");
+    const counterValEl = clonedNode.getElementById("click-counter-val");
+    btnEl.addEventListener("click", (e) => {
+      e.preventDefault();
+      containerEl.classList.toggle("red");
+      this.clickCounter++;
+      counterValEl.textContent = this.clickCounter;
+    });
+
+    // Create a new shadow DOM's root
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    // Append the template content into the shadow DOM
+    shadowRoot.appendChild(clonedNode);
+  }
 }
 
-customElements.define('my-paragraph', MyParagraph)
+// Define <my-paragraph> element
+customElements.define("my-paragraph", MyParagraph);
